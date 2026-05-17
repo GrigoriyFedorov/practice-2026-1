5b7bfe60c5cfb039e488ad0ff1c74b922bd53cba
Grigoriy Fedorov <grisha.2004.aaa@gmail.com>
2026-05-17 14:42:44 +0300

    Создал конструктор для DataBase, создающий директорию

--- src/MyOwnDatabase/DataBase.cs
+++ src/MyOwnDatabase/DataBase.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyOwnDatabase
 {
-    public class Class1
+    public class DataBase
     {
+        private readonly string _rootPath;
+        public DataBase(string rootPath)
+        {
+            _rootPath = rootPath;
+
+            if (!Directory.Exists(_rootPath))
+                Directory.CreateDirectory(rootPath);
+        }
     }
 }
